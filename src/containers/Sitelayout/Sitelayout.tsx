346699e996efb5e39c01212f2ea9c99d666c0ef8
aleenageorge117 <aleenageorge117@gmail.com>
2022-02-06 17:32:15 -0500

    feat: added assets files, Sitelayout component, Routes
    Bootstrap, font, js files added in assets; Sitelayout component added; Basic route setup

---
+++ src/containers/Sitelayout/Sitelayout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//COMPONENTS
+
+// SCSS
+import "./Sitelayout.scss";
+
+const SiteLayout = () => {
+    return (
+        <div>
+            sitelayout
+        </div>
+    );
+}
+
+export default SiteLayout;
